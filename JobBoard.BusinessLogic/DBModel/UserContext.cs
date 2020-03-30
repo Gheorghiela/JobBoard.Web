7750060492b882eda8887105546416f1ff501f65
Georgeta Ciorba <georgeta.ciorba@isa.utm.md>
2020-03-30 22:42:22 +0300

    EntityFramework first commit

---
+++ JobBoard.BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using JobBoard.Domain.Entities.User;
+
+namespace JobBoard.BusinessLogic.DBModel
+{
+    class UserContext : DbContext
+    {
+        public UserContext():
+            base("name=eUseControl") //connectionstring name define in your web.confib
+        {
+
+        }
+
+        public virtual DbSet<UDbTable> Users { get; set; }
+    }
+}
---
+++ JobBoard.Domain/Entities/User/UDbTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Domain.Entities.User
+{
+    public class UDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(30, MinimumLength = 4, ErrorMessage = "Username must be less than 30 characters")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "Password must be more than 8 characters")]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+         public DateTime LastLogin { get; set; }
+
+        [StringLength(30)]
+        public string LastIp { get; set; }
+
+        public URole Level { get; set; }
+
+    }
+}
